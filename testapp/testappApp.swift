338c854657a886b4ad3655187ecd7c265b6c6712
manel kacem <manelkacem11@gmail.com>
2024-12-23 13:23:00 +0100

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Orangeodc10 on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
